336cef8051f94595e313a3e39bf1f02dfcff4d5d
Daniel Pham <dnypham@gmail.com>
2022-04-20 16:53:43 -0700

    Add functionality to select a rating.

--- interactive-rating-component-main/src/components/interactive-rating.js
+++ interactive-rating-component-main/src/components/interactive-rating.js
@@ -1,33 +1,45 @@
 import React, { useState, useEffect } from "react";
 
 export default function InteractiveRating() {
+  const [selectedRating, setSelectedRating] = useState(null);
+
   const createArray = length => [
     ...Array(length)
   ];
 
   const renderRatings = () => {
-    console.log(createArray(5))
     return createArray(5).map((n, i) => (
-      <button className="rating-button overpass-400" key={i}>{i + 1}</button>
+      <button key={i} className={`rating-button overpass-400 ${selectedRating === i + 1 ? 'active' : ''}`} onClick={() => setSelectedRating(i + 1)}>{i + 1}</button>
     ))
   };
 
+  const selectRating = () => {
+
+  }
+
+  const submitRating = () => {
+    event.preventDefault();
+  }
+
+  console.log(selectedRating);
   return (
-    <div className="interactive-rating-container">
-      <div className="star-container">
-        <img src="./assets/icon-star.svg" />
+    <form onSubmit={submitRating}>
+      <div className="interactive-rating-container">
+        <div className="star-container">
+          <img src="./assets/icon-star.svg" />
+        </div>
+        <h1 className="overpass-700 interactive-rating-header">How did we do?</h1>
+        <p className="overpass-400 interactive-rating-paragraph">
+          Please let us know how we did with your support request.
+          All feedback is appreciated to help us improve our offering!
+        </p>
+        <div className="rating-buttons-container">
+          {renderRatings()}
+        </div>
+        <div className="submit-button-container">
+        <button className="submit-button overpass-400" type="submit">SUBMIT</button>
+        </div>
       </div>
-      <h1 className="overpass-700 interactive-rating-header">How did we do?</h1>
-      <p className="overpass-400 interactive-rating-paragraph">
-        Please let us know how we did with your support request.
-        All feedback is appreciated to help us improve our offering!
-      </p>
-      <div className="rating-buttons-container">
-        {renderRatings()}
-      </div>
-      <div className="submit-button-container">
-      <button className="submit-button overpass-400" type="submit">SUBMIT</button>
-      </div>
-    </div>
+    </form>
   )
 }
